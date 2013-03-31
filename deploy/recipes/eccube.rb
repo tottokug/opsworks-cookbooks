c465ee4d58f3ac832b67aa7eb374cca1e6015993
tottokug <magcot.com+git@gmail.com>
2013-03-31 09:13:17 +0900

    logging

--- deploy/recipes/eccube.rb
+++ deploy/recipes/eccube.rb
@@ -15,12 +15,14 @@ node[:deploy].each do |application, deploy|
   opsworks_deploy_dir do
     user deploy[:user]
     group "apache"
+    Chef::Log.info("tottokug-Log deploy_to = #{deploy[:deploy_to]}  ")
     path deploy[:deploy_to]
     mode "0775"
     recursive true
   end
 
   opsworks_deploy do
+    Chef::Log.info("tottokug-Log opsworks_deploy do ")
     deploy_data deploy
     app application
   end
