b5a82825b412f97b8767ab30380fc4d0e53d3b7c
rtokugami <r.tokugami@miningbrownie.co.jp>
2013-04-01 09:43:17 +0900

    chmdo script

--- deploy/recipes/eccube.rb
+++ deploy/recipes/eccube.rb
@@ -1,29 +1,12 @@
 #
 # Cookbook Name:: deploy
-# Recipe:: php
+# Recipe:: eccube
 #
-
-include_recipe "mod_php5_apache2"
-include_recipe "mod_php5_apache2::php"
 
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'php'
     Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
     next
-  end
-
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group] 
-    Chef::Log.info("tottokug-Log deploy_to = #{deploy[:deploy_to]}  ")
-    path deploy[:deploy_to]
-    mode "0755"
-  end
-
-  opsworks_deploy do
-    Chef::Log.info("tottokug-Log opsworks_deploy do ")
-    deploy_data deploy
-    app application
   end
 
   script "chmod_applicaton" do
@@ -32,13 +15,10 @@ node[:deploy].each do |application, deploy|
     cwd deploy[:deploy_to]
     user "root"
     code <<-EOH
-    chmod -R 775 current/html
-    chown -R deploy:apache current/html
-    chmod -R 775 current/data
-    chown -R deploy:apache current/data
+    chmod -R 775 current/html current/data
+    chown -R deploy:apache current/html current/data
     EOH
   end
-
 
 end
 
